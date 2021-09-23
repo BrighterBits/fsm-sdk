4dc99f4d92db2b360ef2d9f0b6c734ddb0631948
Brian D. Wong <bdwong.net@gmail.com>
2021-09-23 08:50:19 -0700

    Add DTO version for UdoMeta

--- src/test/client-public-api.spec.ts
+++ src/test/client-public-api.spec.ts
@@ -78,7 +78,7 @@ describe('compatibility', () => {
 
     describe('ALL_DTO_VERSIONS', () => {
         it('should have DTOs', () => {
-            assert.strictEqual(Object.keys(fsm.ALL_DTO_VERSIONS).length, 79);
+            assert.strictEqual(Object.keys(fsm.ALL_DTO_VERSIONS).length, 80);
         });
 
     });
